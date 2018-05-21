730ea5f8a67fc1cc243ba559c9e4be2edfc2f288
peilun <peilun2050@gmail.com>
2018-05-21 11:58:53 +0800

    first commit

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_blog',
         'USER': 'root',
-        'PASSWORD': '5849312',
+        'PASSWORD': 'vie1314',
         'HOST': '127.0.0.1',
         'PORT': '3306'
     }
@@ -126,6 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/django-blog/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
